d71be0b68d19e983ef774f0f26842cf5668ea89b
llyes <daoudllyes@hotmail.fr>
2026-01-28 10:35:25 +0100

    fix: next config + routing works locally

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
 export default nextConfig;
+
