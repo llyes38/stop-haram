8cb465ef9245c47687ebe5d5318bdbca14a9757d
llyes <daoudllyes@hotmail.fr>
2026-02-02 21:35:22 +0100

    Vibration sur les notifs push (sw.js)

--- public/sw.js
+++ public/sw.js
@@ -16,6 +16,8 @@ self.addEventListener("push", (event) => {
     tag: data.tag || "stopharam-push",
     requireInteraction: !!data.requireInteraction,
     data: { url: data.url || "/" },
+    vibrate: data.vibrate || [200, 100, 200],
+    silent: !!data.silent,
   };
   event.waitUntil(self.registration.showNotification(title, options));
 });
