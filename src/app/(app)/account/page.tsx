b93f0e123ff9ff965561c910c5c2c2c43e3765e6
llyes <daoudllyes@hotmail.fr>
2026-02-06 23:07:17 +0100

    Libellé: Révoquer l'accès  Se déconnecter

--- src/app/(app)/account/page.tsx
+++ src/app/(app)/account/page.tsx
@@ -509,7 +509,7 @@ export default function AccountPage() {
                   <svg className="h-5 w-5 text-white/50 shrink-0" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                   </svg>
-                  Révoquer l&apos;accès sur cet appareil
+                  Se déconnecter
                 </button>
               )}
             </div>
